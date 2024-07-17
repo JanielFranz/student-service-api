2a948394bcff18af3874b0c62cf39f0d749fd107
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-07-16 23:27:36 -0500

    feat(rest): delete and put added

--- src/main/java/com/franz/students/app/infrastructure/adapters/input/rest/StudentRestAdapter.java
+++ src/main/java/com/franz/students/app/infrastructure/adapters/input/rest/StudentRestAdapter.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,8 +33,25 @@ public class StudentRestAdapter {
 
     @PostMapping("/v1/students")
     public ResponseEntity<StudentResponse> create(@Valid @RequestBody StudentCreateRequest request){
+
         //Transform a request to student, then student to a response
         StudentResponse response = studentRestMapper.toStudentResponseFromStudent(studentRestMapper.toStudentFromRequest(request));
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+//        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        //returning the new url of the resource (better practice)
+        return ResponseEntity.created(URI.create("api/v1/students".concat(response.getId().toString()))).body(response);
+    }
+
+    @PutMapping("/v1/students/{id}")
+    public StudentResponse update(@PathVariable Long id, @Valid @RequestBody StudentCreateRequest request){
+        StudentResponse response =  studentRestMapper
+                .toStudentResponseFromStudent(studentInputPort.update(id, studentRestMapper.toStudentFromRequest(request)));
+        return response;
+    }
+
+    @DeleteMapping("v1/students/{id}")
+    public void delete(@PathVariable Long id){
+        studentInputPort.deleteById(id);
     }
 }
